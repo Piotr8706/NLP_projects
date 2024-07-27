2ddc4704f37480caffd52cdc5a6a2174984b63d0
piotr8706 <piotr.beldowski.pb@gmail.com>
2024-07-27 15:36:21 +0200

    Text summary

--- text_summary.py
+++ text_summary.py
@@ -9,3 +9,25 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 nltk.download('punkt')
 nltk.download('stopwords')
+
+"""
+This program summarizes text using TextRank Algorithm
+"""
+
+def wrap():
+    """
+    
+    """
+    pass
+
+def summary():
+    """
+    Summarization of the text
+    """
+    pass
+
+def main():
+    df = pd.read_csv('C:\Users\piotr\OneDrive\Pulpit\Kaggle\jigsaw-unintended-bias-in-toxicity-classification\all_data.csv')
+
+if __name__=='__main__':
+    main()
