00fc30a217e2800de18b54ae24c1fce6bea23b6d
piotr8706 <piotr.beldowski.pb@gmail.com>
2024-07-31 18:25:14 +0200

    Testing

--- text_summary.py
+++ text_summary.py
@@ -66,7 +66,35 @@ def generate_summary(text, num_sentences=5):
     return summary
 
 def main():
-    df = pd.read_csv('C:\Users\piotr\OneDrive\Pulpit\Kaggle\jigsaw-unintended-bias-in-toxicity-classification\all_data.csv')
+    #df = pd.read_csv('C:\Users\piotr\OneDrive\Pulpit\Kaggle\jigsaw-unintended-bias-in-toxicity-classification\all_data.csv')
+    text = """Many of us, which are working in tech industry, are asking the same question, back in 2023 and now in 2024. When the tech jobs are coming back and to what extent?
+
+    Long story short, the most simple and probably accurate answer is, when interest rates drop and money gets cheap again. But, there are couple of additional arguments that we need to take into account, that were not present before, like in previous crises that happened in 2001 and 2008.
+
+    Beggining of the year 2024, does not have good news, as we all saw and is continues the same trend as past year 2023. We all testify, that getting job in 2024, especially junor roles is much worse and harder as it was back in 2023.
+
+    When we ask ourselves the question in title, we mostly think that tech jobs refer to software development jobs in general, but that is true to some extent. Things have changed in past year. Technology is not only about software development, but it is generally about process of things getting better. Like things getting cheaper or new innovations kick in, like in hardware world.
+
+    So you might ask yourself, what are those things, that have changed tech jobs industry, beside cheap money and high interest rates? Let me ask you a question, what technology have skyrocketed in past year or year and half? Yes you know the answer, it is AI.
+
+    So, the classical or traditional way of investments in software development, may not play huge role to improve technology as we used to see in past two decades. Like me or anybody else, huge excitement and expectation comes from AI, either from software development side or new AI no code tools, robotics etc.
+
+    When we talk about software development jobs, back in 1990s, most of web development jobs considered writing HTML and that was the whole career of web developer before. Manual QA was also impacted with automation tools, Server management for IT where we managed individual servers, now is swapped out with cloud, and what was manual work is swapped out with of the shelf stuff offered by cloud providers.
+
+    When we talk about low code or nocode tools, they are now supercharged with AI tools or extensions like FlutterFlow, Supabase AI, GitHub Copilot etc., that are lowering the amount of work that needs to be done from our developer side. When we look at this from other side, this means that tech skills needed to be productive are shrinking. This probably means that this shrinking will also put price pressure on salaries that we used to see before in tech job market.
+
+    So, what this all means for us as software developers and our future?
+
+    First hings first, all this does not mean that all jobs are going away like you may read nowdays. It might, take years and decades to implement, improve and then deploy all those advancements in AI to reality, either software advancements or robotics.
+
+    Second, this crisis in tech industry, layoffs and advancements in AI means we need to acquire new skillset for future tech jobs, that might not look the same way as we are used to. New skillset might be skillset in data science, robotics or new roles that AI might create in future. I know, that going from software development role in web or mobile to data science or robotics is not trivial, but it is what it is.
+
+    So, the conclusion is, do not get depressed about current environment of market. We all are in the same boat. Thinking about our future and paying attention to the job market and checking what is going on is not a bad idea, just to get insight of what will be new trends, when this market will recover from current crisis, do we need and what are our transition plans. Hopefully we will be on track by the end of this year.
+
+    If you liked this story, leave a clap, leave a comment and stay tuned for more."""
+    source = "https://mehobega.medium.com/when-will-the-tech-jobs-come-back-a563d5ba45ba"
+    result = generate_summary(text)
+    print(result)
 
 if __name__=='__main__':
     main()
